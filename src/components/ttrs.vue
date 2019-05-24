554a9837697ba240c615ebc920ff02c024138a1e
ivgtr <gear.1112t.o@gmail.com>
2019-05-24 18:24:13 +0900

    1stcommit

---
+++ src/components/ttrs.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="ttrs">
+    <table>
+      <tr v-for="hei in height" :key="hei.id">
+        <td v-for="wid in width" :key="wid.id"></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      width:[
+        " "," "," "," "," "," "," "," "," "," ",
+      ],
+      height:[
+        " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",
+      ],
+      intervalId: undefined
+    }
+  },
+    mounted () {
+      var array = [
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ];
+
+
+      drow()
+      this.intervalId = setInterval(function () {
+        drow()
+      }, 1000)
+    },
+    beforeDestroy () {
+      console.log('clearInterval')
+      clearInterval(this.intervalId)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+table {
+    margin: 0 auto; /* 中央寄せ */
+    width: 200px;
+    height: 400px;
+}
+td {
+    width: 10%; /* 横10マス */
+    height: 5%; /* 縦20マス */
+}
+.default {
+    background-color: #455A64;
+}
+.stick {
+    background-color: #03A9F4;
+}
+</style>
---
+++ src/views/Game.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="game">
+    <h1>This is an about page</h1>
+    <ttrs />
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import ttrs from '@/components/ttrs.vue'
+
+export default {
+  name: 'game',
+  components: {
+    ttrs
+  }
+}
+</script>
